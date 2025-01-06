5fd1e6725c95f1b5af64c90784b7844e54062f6c
Hina Zafar <hina.nida@gmail.com>
2025-01-06 21:16:10 +0500

    Initial commit

---
+++ src/test/java/ConditionalMethods.java
@@ -0,0 +1,7 @@
+public class ConditionalMethods {
+    public static void main(String[] args) {
+        // Conditional Methods/commands can be accessed through webElements
+        // returns boolean value (true/false)
+
+    }
+}
