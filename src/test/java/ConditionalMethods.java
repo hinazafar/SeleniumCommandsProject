22a8662283af98608a0a0a2ca885dcf2a5a569d2
Hina Zafar <hina.nida@gmail.com>
2025-01-07 17:21:34 +0500

    This code will show the Selected status of a webElement. if the element is selected or not . mostly used for radio buttons/check boxes/dropdown

--- src/test/java/ConditionalMethods.java
+++ src/test/java/ConditionalMethods.java
@@ -11,12 +11,31 @@ public class ConditionalMethods {
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.nopcommerce.com/register");
         driver.manage().window().maximize();
-        //WebElement logo = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']"));
-        //if the element is displayed or not
-        //System.out.println("Display Status of logo is: "+logo.isDisplayed());
 
+        //isDisplayed()
+        WebElement logo = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']"));
+        //if the element is displayed or not
+        System.out.println("Display Status of logo is: "+logo.isDisplayed());
+
+        //isEnabled
         WebElement fname = driver.findElement(By.xpath("//input[@id='FirstName']"));
+        //if the element is enabled/operational or disabled/inactive
         System.out.println("First Name's Enabled status is: " + fname.isEnabled());
 
+        //isSelected()
+        WebElement male_rb = driver.findElement(By.xpath("//input[@id='gender-male']"));
+        WebElement female_rb = driver.findElement(By.xpath("//input[@id='gender-female']"));
+        //if the element is selected or not . mostly used for radio buttons/check boxes/dropdown
+        //before selection
+        System.out.println("Before Selection");
+        System.out.println("Male Radio Button selected status is: " + male_rb.isSelected());
+        System.out.println("Female Radio Button selected status is: " + female_rb.isSelected());
+
+        //After selection
+        System.out.println("After Selection");
+        male_rb.click();
+        System.out.println("Male Radio Button selected status is: " + male_rb.isSelected());
+        female_rb.click();
+        System.out.println("Female Radio Button selected status is: " + female_rb.isSelected());
     }
 }
