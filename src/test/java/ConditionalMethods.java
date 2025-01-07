a444e8df58cf1dc85840ecce23c92d7f8a0a2772
Hina Zafar <hina.nida@gmail.com>
2025-01-07 16:41:04 +0500

    This code will show the Enabled status of a webElement.

--- src/test/java/ConditionalMethods.java
+++ src/test/java/ConditionalMethods.java
@@ -11,8 +11,12 @@ public class ConditionalMethods {
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.nopcommerce.com/register");
         driver.manage().window().maximize();
-        WebElement logo = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']"));
+        //WebElement logo = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']"));
         //if the element is displayed or not
-        System.out.println("Display Status of logo is: "+logo.isDisplayed());
+        //System.out.println("Display Status of logo is: "+logo.isDisplayed());
+
+        WebElement fname = driver.findElement(By.xpath("//input[@id='FirstName']"));
+        System.out.println("First Name's Enabled status is: " + fname.isEnabled());
+
     }
 }
