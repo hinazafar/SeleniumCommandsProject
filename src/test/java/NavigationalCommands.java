5863905cf6338cf7d63908bc4e9626900e6a8b85
Hina Zafar <hina.nida@gmail.com>
2025-02-05 14:14:16 +0500

    Navigational Commands:

--- src/test/java/NavigationalCommands.java
+++ src/test/java/NavigationalCommands.java
@@ -1,10 +1,19 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class NavigationalCommands {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
 
         WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://www.nopcommerce.com");
+        // Both get() and navigate().to() functional works same but the difference is
+        // get() method only accept the URL as string
+        // whereas navigate().to() method also accept the URL object or the sting as well
+        //driver.get("https://www.nopcommerce.com/");
+        URL url = new URL("https://www.nopcommerce.com");
+        driver.navigate().to(url);
+
     }
 }
