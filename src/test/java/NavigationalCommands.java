e5782e4a9394fa3d1cebb8575c3e75604799a482
Hina Zafar <hina.nida@gmail.com>
2025-02-05 20:47:10 +0500

    Navigational Commands:

--- src/test/java/NavigationalCommands.java
+++ src/test/java/NavigationalCommands.java
@@ -10,10 +10,17 @@ public class NavigationalCommands {
         WebDriver driver = new ChromeDriver();
         // Both get() and navigate().to() functional works same but the difference is
         // get() method only accept the URL as string
-        // whereas navigate().to() method also accept the URL object or the sting as well
+        // whereas navigate().to() method also accept the URL object and the sting as well
         //driver.get("https://www.nopcommerce.com/");
+
         URL url = new URL("https://www.nopcommerce.com");
         driver.navigate().to(url);
+
+        //* How navigate().to() Method Works *//
+        // 1. First it checks if the passed argument is a string or an object
+        // 2. If it is a string format it will directly pass it to get() method
+        // 3. If it is an object(URL) , then it will convert it into string first
+        // and then call the get() method
 
     }
 }
